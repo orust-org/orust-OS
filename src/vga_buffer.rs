9b042ee7c31d99f6ac28cb407041553f54213f5e
Enow Scott <enowscott53@gmail.com>
2024-05-13 20:08:50 +0100

    Implement formating macros

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -1,3 +1,5 @@
+use core::fmt::{Write, Result};
+
 use volatile::Volatile;
 
 #[allow(dead_code)]
@@ -87,14 +89,20 @@ impl Writer {
     }
 }
 
+impl Write for Writer {
+    fn write_str(&mut self, s: &str) -> Result {
+        self.write_string(s);
+        Ok(())
+    }
+}
+
 pub fn print_something() {
     let mut writer = Writer {
         column_position: 0,
         color_code: ColorCode::new(Color::Yellow, Color::Black),
         buffer: unsafe { &mut *(0xb8000 as *mut  Buffer) },
     };
-    writer.write_byte(b'W');
-    writer.write_string("elcome ");
-    writer.write_string("to the ORUST Operating System!");
-
+    writer.write_byte(b'H');
+    writer.write_string("ello ");
+    write!(writer, "The numbers are {} and {}", 42, 1.0/3.0).unwrap();
 }
