dd36163bdd647388d260e03b81b383ae64269434
Enow Scott <enowscott53@gmail.com>
2024-05-13 23:02:04 +0100

    print panic message when we panic

--- src/main.rs
+++ src/main.rs
@@ -5,20 +5,14 @@ mod vga_buffer;
 use core::panic::PanicInfo;
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    use core::fmt::Write;
-    vga_buffer::WRITER.lock().write_str("Hello welcome").unwrap();
-    write!(
-        vga_buffer::WRITER.lock(), 
-        ", some numbers: {} {}", 42, 
-        1.337
-    )
-    .unwrap();
-
+    println!("Hello, Welcome to the ORUST Operating System{}", "!");
+    panic!("Some panic message");
     loop {}
 }
