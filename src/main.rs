20f233ad037967f03bc4f1449f2746e6aa925319
Enow Scott <enowscott53@gmail.com>
2024-05-13 20:38:34 +0100

    added newline capability

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ fn panic(_info: &PanicInfo) -> ! {
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-     vga_buffer::print_something();
+    vga_buffer::print_something();
 
     loop {}
 }
--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -85,7 +85,24 @@ impl Writer {
     }
 
     fn new_line(&mut self) {
-        todo!()
+        or row in 1..BUFFER_HEIGHT {
+            for col in 0..BUFFER_WIDTH {
+                let character = self.buffer.chars[row][col].read();
+                self.buffer.chars[row - 1][col].write(character);
+            }
+        }
+        self.clear_row(BUFFER_HEIGHT - 1);
+        self.column_position = 0;
+    }
+
+    fn clear_row(&mut self, row: usize) {
+        let blank = ScreenChar {
+            ascii_character: b' ',
+            color_code: self.color_code,
+        };
+        for col in 0..BUFFER_WIDTH {
+            self.buffer.chars[row][col].write(blank);
+        }
     }
 }
 
