7ae2e152f8b6c43a404667ab1a083a3638cd6fc5
Enow Scott <enowscott53@gmail.com>
2024-05-15 14:07:37 +0100

    added testable trait, cleaned up clippy warnings

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,21 @@ mod serial;
 
 use core::panic::PanicInfo;
 
+pub trait Testable {
+    fn run(&self);
+}
+
+impl<T> Testable for T
+where
+    T: Fn(),
+{
+    fn run(&self){
+        serial_print!("{}...\t", core::any::type_name::<T>());
+        self();
+        serial_println!("[ok]");
+    }
+}
+
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[repr(u32)]
 pub enum QemuExitCode {
@@ -43,27 +58,24 @@ fn panic(info: &PanicInfo) -> ! {
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    println!("Hello, Welcome to the ORUST Operating System{}", "!");
 
     #[cfg(test)]
     test_main();
     
+    #[allow(clippy::empty_loop)]
     loop {}
 }
 
 #[cfg(test)]
-fn test_runner(tests: &[&dyn Fn()]) {
+fn test_runner(tests: &[&dyn Testable]) {
     serial_println!("Running {} tests", tests.len());
     for test in tests {
-        test();
+        test.run();
     }
     exit_qemu(QemuExitCode::Success);
 }
 
 #[test_case]
 fn trivial_assertion() {
-    serial_print!("trivial assertion... ");
     assert_eq!(1, 1);
-    serial_println!("[ok]");
-    loop {}
 }
