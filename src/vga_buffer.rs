9d931de83624746d4bac7d32d90edbb874f872d7
Enow Scott <enowscott53@gmail.com>
2024-05-15 14:20:24 +0100

    added vga tests

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -139,3 +139,25 @@ macro_rules! println {
 pub fn _print(args: Arguments) {
     WRITER.lock().write_fmt(args).unwrap();
 }
+
+#[test_case]
+fn test_println_simple() {
+    println!("test_println_simple output");
+}
+
+#[test_case]
+fn test_println_many() {
+    for _ in 0..200 {
+        println!("test_println_many output");
+    }
+}
+
+#[test_case]
+fn test_println_output() {
+    let s = "Some test string that fits on a single line";
+    println!("{s}");
+    for (i, c) in s.chars().enumerate() {
+        let screen_char = WRITER.lock().buffer.chars[BUFFER_HEIGHT - 2][i].read();
+        assert_eq!(char::from(screen_char.ascii_character), c);
+    }
+}
