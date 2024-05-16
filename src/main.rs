ca6f1df2807ab48c3da07a32ce6402a0e2ec1d86
Enow Scott <enowscott53@gmail.com>
2024-05-16 08:00:50 +0100

    got integration test working

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,24 @@
 #![no_std]
 #![no_main]
 #![feature(custom_test_frameworks)]
-#![test_runner(crate::test_runner)]
+#![test_runner(orust_os::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
-mod vga_buffer;
-mod serial;
-
 use core::panic::PanicInfo;
+use orust_os::println;
 
 pub trait Testable {
     fn run(&self);
 }
 
-impl<T> Testable for T
-where
-    T: Fn(),
-{
-    fn run(&self){
-        serial_print!("{}...\t", core::any::type_name::<T>());
-        self();
-        serial_println!("[ok]");
-    }
-}
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    println!("Hello, Welcome to the ORUST Operating System{}", "!");
+    #[cfg(test)]
+    test_main();
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-#[repr(u32)]
-pub enum QemuExitCode {
-    Success = 0x10,
-    Failed = 0x11,
-}
-
-pub fn exit_qemu(exit_code: QemuExitCode) {
-    use x86_64::instructions::port::Port;
-
-    unsafe {
-        let mut port = Port::new(0xf4);
-        port.write(exit_code as u32);
-    }
+    #[allow(clippy::empty_loop)]
+    loop {}
 }
 
 #[cfg(not(test))]
@@ -50,32 +31,5 @@ fn panic(info: &PanicInfo) -> ! {
 #[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    serial_println!("[failed]\n");
-    serial_println!("Error: {}\n", info);
-    exit_qemu(QemuExitCode::Failed);
-    loop {}
-}
-
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
-
-    #[cfg(test)]
-    test_main();
-    
-    #[allow(clippy::empty_loop)]
-    loop {}
-}
-
-#[cfg(test)]
-fn test_runner(tests: &[&dyn Testable]) {
-    serial_println!("Running {} tests", tests.len());
-    for test in tests {
-        test.run();
-    }
-    exit_qemu(QemuExitCode::Success);
-}
-
-#[test_case]
-fn trivial_assertion() {
-    assert_eq!(1, 1);
+    orust_os::test_panic_handler(info)
 }
---
+++ tests/basic_boot.rs
@@ -0,0 +1,26 @@
+#![no_std]
+#![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(orust_os:test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+use core::panic::PanicInfo;
+use orust_os::println;
+
+#[no_mangle] 
+pub extern "C" fn _start() -> ! {
+    test_main();
+
+    #[allow(clippy::empty_loop)]
+    loop {}
+}
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    orust_os::test_panic_handler(info)
+}
+
+#[test_case]
+fn test_println() {
+    println!("test_println output");
+}
