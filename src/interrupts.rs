4b0af8b12ada33d44e0dbf0a42cec83960ee6080
Enow Scott <148756598+Blindspot22@users.noreply.github.com>
2024-06-12 17:26:33 +0100

    page fault handler, and displaying lev4 page location.

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -1,8 +1,9 @@
 use crate::println;
 use crate::gdt;
+use crate::hlt_loop;
 
 use lazy_static::lazy_static;
-use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
+use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame, PageFaultErrorCode};
 use pic8259::ChainedPics;
 use spin;
 
@@ -14,6 +15,7 @@ lazy_static! {
     static ref IDT: InterruptDescriptorTable = {
         let mut idt = InterruptDescriptorTable::new();
         idt.breakpoint.set_handler_fn(breakpoint_handler);
+        idt.page_fault.set_handler_fn(page_fault_handler);
         unsafe {
             idt.double_fault.set_handler_fn(double_fault_handler)
                 .set_stack_index(gdt::DOUBLE_FAULT_IST_INDEX); // new
@@ -50,3 +52,16 @@ pub const PIC_2_OFFSET: u8 = PIC_1_OFFSET + 8;
 
 pub static PICs: spin::Mutex<ChainedPics> =
     spin::Mutex::new(unsafe {ChainedPics::new(PIC_1_OFFSET, PIC_2_OFFSET)});
+
+    extern "x86-interrupt" fn page_fault_handler(
+        stack_frame: InterruptStackFrame,
+        error_code: PageFaultErrorCode,
+    ) {
+        use x86_64::registers::control::Cr2;
+    
+        println!("EXCEPTION: PAGE FAULT");
+        println!("Accessed Address: {:?}", Cr2::read());
+        println!("Error Code: {:?}", error_code);
+        println!("{:#?}", stack_frame);
+        hlt_loop();
+    }
--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,11 @@ pub extern "C" fn _start() -> ! {
 
     orust_os::init();
 
+    use x86_64::registers::control::Cr3;
+
+    let (level_4_page_table, _) = Cr3::read();
+    println!("Level 4 page table at: {:?}", level_4_page_table.start_address());
+
     #[cfg(test)]
     test_main();
 
