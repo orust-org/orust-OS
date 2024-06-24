45f50986545e13cf432a61dcc9e42162dc806c1e
Enow Scott <148756598+Blindspot22@users.noreply.github.com>
2024-06-24 15:04:08 +0100

    paging

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -1,8 +1,9 @@
 use crate::{print, println};
 use crate::gdt;
+use crate::hlt_loop;
 
 use lazy_static::lazy_static;
-use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
+use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame, PageFaultErrorCode};
 use pic8259::ChainedPics;
 use spin;
 
@@ -33,6 +34,7 @@ lazy_static! {
     static ref IDT: InterruptDescriptorTable = {
         let mut idt = InterruptDescriptorTable::new();
         idt.breakpoint.set_handler_fn(breakpoint_handler);
+        idt.page_fault.set_handler_fn(page_fault_handler);
         unsafe {
             idt.double_fault.set_handler_fn(double_fault_handler)
                 .set_stack_index(gdt::DOUBLE_FAULT_IST_INDEX); // new
@@ -110,4 +112,16 @@ extern "x86-interrupt" fn keyboard_interrupt_handler(
         PICS.lock()
             .notify_end_of_interrupt(InterruptIndex::Keyboard.into());
     }
+}
+extern "x86-interrupt" fn page_fault_handler(
+    stack_frame: InterruptStackFrame,
+    error_code: PageFaultErrorCode)
+{
+    use x86_64::registers::control::Cr2;
+    println!("EXCEPTION: PAGE FAUL");
+    println!("Accessed Adress: {:?}", Cr2::read());
+    println!("Error Code: {error_code:?}");
+    println!("{stack_frame:#?}");
+
+    hlt_loop();
 }
