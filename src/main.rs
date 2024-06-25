c24d7536421872bbdaa956848b5a6c24805282c3
Enow Scott <148756598+Blindspot22@users.noreply.github.com>
2024-06-25 09:32:10 +0100

    use the crate-provided memory translation

--- src/main.rs
+++ src/main.rs
@@ -17,13 +17,14 @@ pub trait Testable {
 entry_point!(kernel_main);
 
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
-    use orust_os:: memory::translate_addr;
-    use x86_64::VirtAddr;
+    use orust_os:: memory;
+    use x86_64::{structures::paging::Translate, VirtAddr};
 
     println!("Hello, Welcome to the ORUST Operating System{}", "!");
     orust_os::init();
 
     let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    let mapper = unsafe { memory::init(phys_mem_offset) };
     let addresses = [ 
         0xb8000, 
         0x201008, 
@@ -33,7 +34,7 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
 
     for &address in &addresses {
         let virt = VirtAddr::new(address);
-        let phys = unsafe { translate_addr(virt, phys_mem_offset) };
+        let phys = mapper.translate_addr(virt);
         println!("{:?} -> {:?}", virt, phys);
     }
 
--- src/memory.rs
+++ src/memory.rs
@@ -1,9 +1,14 @@
 use x86_64::{
-    structures::paging::PageTable,
-    VirtAddr, PhysAddr
+    structures::paging::{PageTable, OffsetPageTable},
+    VirtAddr
 };
 
-pub unsafe fn active_level_4_table(physical_memory_offset: VirtAddr)
+pub unsafe fn init(physical_memory_offset: VirtAddr) -> OffsetPageTable<'static> {
+    let level_4_table = active_level_4_table(physical_memory_offset);
+    OffsetPageTable::new(level_4_table, physical_memory_offset)
+}
+
+unsafe fn active_level_4_table(physical_memory_offset: VirtAddr)
     -> &'static mut PageTable
 {
     use x86_64::registers::control::Cr3;
@@ -15,39 +20,4 @@ pub unsafe fn active_level_4_table(physical_memory_offset: VirtAddr)
     let page_table_ptr: *mut PageTable = virt.as_mut_ptr();
 
     &mut *page_table_ptr 
-}
-
-pub unsafe fn translate_addr(addr: VirtAddr, physical_memory_offset: VirtAddr)
-    -> Option<PhysAddr>
-{
-    translate_addr_inner(addr, physical_memory_offset)
-}
-
-pub fn translate_addr_inner(addr: VirtAddr, physical_memory_offset: VirtAddr)
--> Option<PhysAddr>
-{
-use x86_64::structures::paging::page_table::FrameError;
-use x86_64::registers::control::Cr3;
-
-let (level_4_table_frame, _) = Cr3::read();
-
-    let table_indexes = [
-        addr.p4_index(), addr.p3_index(), addr.p2_index(), addr.p1_index()
-    ];
-    let mut frame = level_4_table_frame;
-
-    for &index in &table_indexes {
-        let virt = physical_memory_offset + frame.start_address().as_u64();
-        let table_ptr: *const PageTable = virt.as_ptr();
-        let table = unsafe {&*table_ptr};
-
-        let entry = &table[index];
-        frame = match entry.frame() {
-            Ok(frame) => frame,
-            Err(FrameError::FrameNotPresent) => return None,
-            Err(FrameError::HugeFrame) => panic!("huge pages not supported"),
-        };
-    }
-
-    Some(frame.start_address() + u64::from(addr.page_offset()))
 }
