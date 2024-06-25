4a4c96d9da813ff6ef2dea1dabd9a3c84875af7e
Enow Scott <148756598+Blindspot22@users.noreply.github.com>
2024-06-25 08:54:37 +0100

    translating virt to phys example

--- src/main.rs
+++ src/main.rs
@@ -17,32 +17,25 @@ pub trait Testable {
 entry_point!(kernel_main);
 
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
-    use orust_os:: memory:: active_level_4_table;
+    use orust_os:: memory::translate_addr;
     use x86_64::VirtAddr;
-    use x86_64::structures::paging::PageTable;
 
     println!("Hello, Welcome to the ORUST Operating System{}", "!");
     orust_os::init();
 
     let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
-    let l4_table = unsafe { active_level_4_table(phys_mem_offset) };
+    let addresses = [ 
+        0xb8000, 
+        0x201008, 
+        0x0100_0020_1a10, 
+        boot_info.physical_memory_offset
+    ];
 
-    for (i, entry) in l4_table.iter().enumerate() {
-        if !entry.is_unused() {
-            println!("L4 Entry {}: {:?}", i, entry);
-
-            let phys = entry.frame().unwrap().start_address();
-    let virt = phys.as_u64() + boot_info.physical_memory_offset;
-    let ptr = VirtAddr::new(virt).as_mut_ptr();
-    let l3_table: &PageTable = unsafe { &*ptr };
-
-    for (i, entry) in l3_table.iter().enumerate() {
-        if !entry.is_unused() {
-            println!("  L3 Entry {}: {:?}", i, entry);
-        }
+    for &address in &addresses {
+        let virt = VirtAddr::new(address);
+        let phys = unsafe { translate_addr(virt, phys_mem_offset) };
+        println!("{:?} -> {:?}", virt, phys);
     }
-}
-}
 
     #[cfg(test)]
     test_main();
