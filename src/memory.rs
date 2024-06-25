1d1bd70cc8eea49ee0af9291784492c8c62dfdb2
Enow Scott <148756598+Blindspot22@users.noreply.github.com>
2024-06-25 18:46:55 +0100

    fix

--- src/memory.rs
+++ src/memory.rs
@@ -1,6 +1,8 @@
 use x86_64::{
-    structures::paging::{PageTable, OffsetPageTable},
-    VirtAddr
+    structures::paging::{
+        Page, PhysFrame, Mapper, Size4KiB, FrameAllocator, PageTable, OffsetPageTable
+    },
+    PhysAddr, VirtAddr
 };
 
 pub unsafe fn init(physical_memory_offset: VirtAddr) -> OffsetPageTable<'static> {
@@ -20,4 +22,29 @@ unsafe fn active_level_4_table(physical_memory_offset: VirtAddr)
     let page_table_ptr: *mut PageTable = virt.as_mut_ptr();
 
     &mut *page_table_ptr 
+}
+
+pub fn create_example_mapping(
+    page: Page,
+    mapper: &mut OffsetPageTable,
+    frame_allocator: &mut impl FrameAllocator<Size4KiB>,
+) {
+    use x86_64::structures::paging::PageTableFlags as Flags;
+
+    let frame = PhysFrame::containing_address(PhysAddr::new(0xb8000));
+    let flags = Flags::PRESENT | Flags::WRITABLE;
+
+    let map_to_result = unsafe {
+        // FIXME: this is not safe, we do it only for testing
+        mapper.map_to(page, frame, flags, frame_allocator)
+    };
+    map_to_result.expect("map_to failed").flush();
+}
+
+pub struct EmptyFrameAllocator;
+
+unsafe impl FrameAllocator<Size4KiB> for EmptyFrameAllocator {
+    fn allocate_frame(&mut self) -> Option<PhysFrame> {
+        None
+    }
 }
