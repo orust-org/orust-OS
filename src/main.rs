25d3f49575cd3416b2a9b1544d46b49003c8ac5b
Blindspot22 <enowscott53@gmail.com>
2024-08-10 13:21:39 +0100

    simple executor runs code

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ use core::panic::PanicInfo;
 
 use orust_os::println;
 
-use alloc::{boxed::Box, vec, vec::Vec, rc::Rc};
+use orust_os::task::{Task, simple_executor::SimpleExecutor};
 
 pub trait Testable {
     fn run(&self);
@@ -37,22 +37,9 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
     allocator::init_heap(&mut mapper, &mut frame_allocator)
         .expect("heap initialization failed");
 
-    // allocate a number on the heap
-    let heap_value = Box::new(41);
-    println!("heap_value at {heap_value:p}");
-
-    let mut vec = Vec::new();
-    for i in 0..500 {
-        vec.push(i);
-    }
-    println!("vec at {:p}", vec.as_slice());
-
-    let reference_counted = Rc::new(vec![1, 2, 3]);
-    let cloned_reference = reference_counted.clone();
-    println!("current reference count is {}", Rc::strong_count(&cloned_reference));
-    core::mem::drop(reference_counted);
-    println!("reference count is {} now", Rc::strong_count(&cloned_reference));
-
+        let mut executor = SimpleExecutor::new();
+    executor.spawn(Task::new(example_task()));
+    executor.run();
 
     #[cfg(test)]
     test_main();
@@ -73,4 +60,13 @@ fn panic(info: &PanicInfo) -> ! {
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     orust_os::test_panic_handler(info)
+}
+
+async fn async_number() -> u32 {
+    42
+}
+
+async fn example_task() {
+    let number = async_number().await;
+    println!("async number: {}", number);
 }
